33256d7b4c3fc836e5e51f17500aca4fdb0d979a
martinspadrna <martinspadrna@gmail.com>
2026-05-04 15:56:32 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const APP_KEY = "rotace_kalkulacky_state_v122";
-const ROTATION_BUILD = "2026-05-04-v.0156-rc-" + Date.now();
+const ROTATION_BUILD = "2026-05-04-v.0157-rc-" + Date.now();
 
 const HARD_MACHINE_HEADERS = ["TNKS01", "TBKR07", "TPKW01", "TPKW02", "TBKR01"];
 const SOFT_MACHINE_HEADERS = ["MSKC01", "MSKC03", "MSKC04", "MFKF06", "MFKF10"];
@@ -1800,7 +1800,7 @@ async function exportCurrentHtml() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "rotace_v.0156-rc.zip";
+    a.download = "rotace_v.0157-rc.zip";
     document.body.appendChild(a);
     a.click();
     a.remove();
