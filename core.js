23b3fa8433f7cdc7f01f16f8758d7b794a9917ad
martinspadrna <martinspadrna@gmail.com>
2026-05-14 23:44:40 +0200

    Add files via upload

--- core.js
+++ core.js
@@ -1,6 +1,6 @@
 
 const APP_KEY = "rotace_kalkulacky_state_v122";
-const APP_VERSION = "v.1.1 (435)";
+const APP_VERSION = "v.1.1 (436)";
 window.APP_VERSION = APP_VERSION;
 const ROTATION_BUILD = "2026-05-12-" + APP_VERSION + "-" + Date.now();
 
