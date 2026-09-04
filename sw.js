8be2bd1fa02f83b9fc3b0af23387b3883535497c
martinspadrna <martinspadrna@gmail.com>
2026-09-04 20:04:24 +0200

    Refine shift report layout

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // RaK development PWA service worker – update-safe build.
-const CACHE_VERSION = 'v1.2-dev-20260904-13';
-const SW_APP_VERSION = 'development 2026-09-04.13';
+const CACHE_VERSION = 'v1.2-dev-20260904-14';
+const SW_APP_VERSION = 'development 2026-09-04.14';
 const STATIC_CACHE = `rotace-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `rotace-runtime-${CACHE_VERSION}`;
 const CORE = ['./', './index.html', './manifest.webmanifest', './assets/app-icons/icon-180.png', './assets/app-icons/icon-192.png', './assets/app-icons/icon-512.png'];
