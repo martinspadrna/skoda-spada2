599856bac7587aaded0b33b6bf9976aa17f8eb5a
martinspadrna <martinspadrna@gmail.com>
2026-09-07 17:12:35 +0200

    Bump development loader to v1.5.11

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ try { if (typeof window.rakMarkModuleReady === 'function') window.rakMarkModuleR
 })();
 
 (async () => {
-  const RAK_MODULE_CACHE_VERSION = "1.5.8";
-  const RAK_DEV_UPDATE_BUILD = "v1.5.8";
+  const RAK_MODULE_CACHE_VERSION = "1.5.11";
+  const RAK_DEV_UPDATE_BUILD = "v1.5.11";
   window.RAK_PWA_BUILD = RAK_DEV_UPDATE_BUILD;
 
   const criticalFiles = [
