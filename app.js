50cd7c29804dda04fa7bbd1f419c017b15303953
martinspadrna <martinspadrna@gmail.com>
2026-09-07 17:45:26 +0200

    Load v1.5.12 operational fixes

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ try { if (typeof window.rakMarkModuleReady === 'function') window.rakMarkModuleR
 })();
 
 (async () => {
-  const RAK_MODULE_CACHE_VERSION = "1.5.11";
-  const RAK_DEV_UPDATE_BUILD = "v1.5.11";
+  const RAK_MODULE_CACHE_VERSION = "1.5.12";
+  const RAK_DEV_UPDATE_BUILD = "v1.5.12";
   window.RAK_PWA_BUILD = RAK_DEV_UPDATE_BUILD;
 
   const criticalFiles = [
@@ -89,7 +89,8 @@ try { if (typeof window.rakMarkModuleReady === 'function') window.rakMarkModuleR
     "rak-shift-report.js",
     "rak-shift-report-entry-fix.js",
     "rak-shift-report-share.js",
-    "brusy-fhb-v158.js"
+    "brusy-fhb-v158.js",
+    "rak-dev-fixes-v1512.js"
   ];
 
   const files = criticalFiles.concat(deferredFiles);
